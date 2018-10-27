38cb8f3c6cf75ebb7c77810b9a33a217dd644b4d
Misael <aguayo.misael@gmail.com>
2018-10-27 15:28:59 -0500

    making changes to main

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ app = Flask("server")
 
 @app.route("/<string:ticker>", methods = ["GET"])
 def index(ticker):
-	try:
+	try: 
 		quandl.ApiConfig.api_key = os.environ.get("QUANDL_KEY")
-		results = quandl.get("EOD/{ticker}".format(ticker))
-	except:
+		results = quandl.get("EOD/{}".format(ticker))
+	except Exception as e:
+		print(e)
 		return jsonify({"error": "Invalid ticker"})
 	# print(results.columns)
 	data = {"data": results.to_json()}
 	return jsonify(data)
 	
+@app.route("/", methods = ["GET"])
+def index2():
+	return "hi"
+
 
 if (__name__ == "__main__"):
 	app.run(host = "0.0.0.0", port = 80, debug = True)
